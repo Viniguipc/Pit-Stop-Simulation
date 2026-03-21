617b5b91e3ad1402b45f0ab65f04d8e3e40e27f7
Viniguipc <138828870+Viniguipc@users.noreply.github.com>
2026-03-21 00:10:47 -0300

    Adicionando funções
    Adicionando funções para impressão do estado dos pneus, verificação do estado dos pneus e escolha do pneu.

--- src/PSS_game.c
+++ src/PSS_game.c
@@ -2,11 +2,36 @@
 #include <time.h>
 #include <stdlib.h>
 
+void imprimir_pneu (int* vet){
+	int i;
+	
+	for (i = 0; i < 4; i++){
+		printf("\nPneu %d - %d", i + 1, vet[i]);
+	}
+}
+
+int estado_pneu (int* vet){
+	int check = 1, i;
+	
+	for (i = 0; i < 4; i++){
+		if (vet[i] == 0){
+			check = 0;
+		}
+	}
+	
+	return check;
+}
+
+void escolhe_pneu (int* op){
+	do{
+		printf("\nEscolha o Pneu a ser trocado (1 a 4): ");
+		scanf("%d", op);
+	}while (*op < 1 || *op > 4);
+}
+
 main(){
-	int i, pneu[4] = {0, 0, 0, 0};
+	int i, op, pneu[4] = {0, 0, 0, 0}, check;
 	
 	printf("Pit Stop Simulation - The Game");
-	for (i = 0; i < 4; i++){
-		printf("\n%d", pneu[i]);
-	}
+
 }
