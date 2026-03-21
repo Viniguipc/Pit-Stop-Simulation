dba36045c6c40ec4aecd946cb8861e966150eb94
Viniguipc <138828870+Viniguipc@users.noreply.github.com>
2026-03-21 00:42:28 -0300

    Adicionando Troca de pneus
    Adicionado a função de troca de pneus e o algoritmo para a troca dos 4 pneus no mais, algum erro de logica tem que ser corrigido nessa função.

--- src/PSS_game.c
+++ src/PSS_game.c
@@ -29,9 +29,51 @@ void escolhe_pneu (int* op){
 	}while (*op < 1 || *op > 4);
 }
 
-main(){
+int trocando_pneu (int op){
+	char button;
+	
+	printf("Aperte \"x\" para desparafusar o pneu %d.", op);
+	scanf("%c", &button);
+	
+	if (button != 'x' && button != 'X'){
+		printf("Falha ao desparafusar o pneu %d! Abandonamos a Corrida DNF.", op);
+	}
+	else{
+		printf("Aperte \"y\" para colocar o pneu novo %d.", op);
+		scanf("%c", &button);
+		
+		if(button != 'y' && button != 'Y'){
+			printf("Falha ao colocar o pneu %d! Abandonamos a Corrida DNF.", op);
+		}
+		else{
+			printf("Aperte \"z\" para parafusar o novo pneu %d", op);
+			scanf("%c", &button);
+			
+			if(button != 'z' && button != 'Z'){
+				printf("Falha ao parafusar o pneu %d! Abandonamos a Corrida DNF.", op);
+			}
+			else{
+				return 1;
+			}
+		}
+	}
+	
+}
+
+int main(){
 	int i, op, pneu[4] = {0, 0, 0, 0}, check;
 	
 	printf("Pit Stop Simulation - The Game");
+	
+	do{
+		imprimir_pneu(pneu);
+		
+		escolhe_pneu(&op);
+		pneu[op - 1] = trocando_pneu(op);
+		
+		check = estado_pneu(pneu);
+	} while (check != 1);
+	
+	printf("\nPit Stop concluido com sucesso!");
 
 }
