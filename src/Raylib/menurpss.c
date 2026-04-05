a0dc65c8c75551036b40ca687f68568b00ff4fe7
Viniguipc <138828870+Viniguipc@users.noreply.github.com>
2026-04-05 19:54:25 -0300

    Menu de fases

--- src/Raylib/menurpss.c
+++ src/Raylib/menurpss.c
@@ -28,7 +28,7 @@
 	BeginDrawing();
 		ClearBackground(BLACK);
 		
-		DrawText("MENU INICIAL", 250, 50, 40, WHITE);
+		DrawText("MENU INICIAL", ((GetScreenWidth() / 2) - (MeasureText("MENU INICIAL)", 40) / 2)), 50, 40, WHITE);
 		
 		for(i = 0; i < 3; i++){
 			if((i + 1) != escolha){
@@ -41,17 +41,23 @@
 	EndDrawing();
 }
 
+//Mudar de lista para carrocel
 void menu_fases(int* op, int fase_atual){
 	char *opcoes[] = {"Iniciar Jogo", "Placar de Tempos", "Sair"};
 	int i, escolha;
 	
 	if(IsKeyPressed(KEY_ENTER)){
-		*op = escolha;
+		if(escolha > fase_atual){
+			
+		}
+		else{
+			*op = escolha;
+		}
 	}
 	else{
 		if(IsKeyPressed(KEY_DOWN)){
 			(escolha)++;
-			if(escolha > 3){
+			if(escolha > 6){
 				escolha = 1;
 			}
 		}
@@ -67,15 +73,26 @@ void menu_fases(int* op, int fase_atual){
 	BeginDrawing();
 		ClearBackground(BLACK);
 		
-		DrawText("MENU INICIAL", 250, 50, 40, WHITE);
+		DrawText("ESCOLHA A FASE", ((GetScreenWidth() / 2) - (MeasureText("ESCOLHA A FASE", 40) / 2)), 50, 40, WHITE);
 		
-		for(i = 0; i < 3; i++){
-			if((i + 1) != escolha){
-				DrawText(opcoes[i], 250, 50 * (i + 2), 30 , WHITE);
+		for(i = 0; i < 6; i++){
+			if((i + 1) <= fase_atual){
+				if((i + 1) != escolha){
+					DrawText(TextFormat("Fase %d - Disponivel", i + 1), ((GetScreenWidth() / 2) - (MeasureText(TextFormat("Fase %d - Disponivel", i + 1), 30) / 2)), 50 * (i + 2), 30 , WHITE);
+				}
+				else{
+					DrawText(TextFormat("Fase %d - Disponivel", i + 1), ((GetScreenWidth() / 2) - (MeasureText(TextFormat("Fase %d - Disponivel", i + 1), 30) / 2)), 50 * (i + 2), 30, BLUE);
+				}
 			}
 			else{
-				DrawText(opcoes[i], 250, 50 * (i + 2), 30, BLUE);
+				if((i + 1) != escolha){
+					DrawText(TextFormat("Fase %d - Bloqueada", i + 1), ((GetScreenWidth() / 2) - (MeasureText(TextFormat("Fase %d - Bloqueada", i + 1), 30) / 2)), 50 * (i + 2), 30 , WHITE);
+				}
+				else{
+					DrawText(TextFormat("Fase %d - Bloqueada", i + 1), ((GetScreenWidth() / 2) - (MeasureText(TextFormat("Fase %d - Bloqueada", i + 1), 30) / 2)), 50 * (i + 2), 30, BLUE);
+				}
 			}
 		}
-	EndDrawing();
+		
+		EndDrawing();
 }
